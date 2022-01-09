87ed0dd6f2ada335aa3acf8e76a4676798a8946d
Susanna-Salata <susanna.kiev@gmail.com>
2022-01-09 16:08:16 +0100

    Shooting Game

---
+++ shooting game.py
@@ -0,0 +1,42 @@
+class Enemy:
+    name = ""
+    lives = 0
+
+    def __init__(self, name, lives):
+        self.name = name
+        self.lives = lives
+
+    def hit(self):
+        self.lives -= 1
+        if self.lives <= 0:
+            print(self.name + ' killed')
+        else:
+            print(self.name + ' has ' + str(self.lives) + ' lives')
+
+
+class Monster(Enemy):
+
+    def __init__(self):
+        super().__init__('Monster', 3)
+
+
+class Alien(Enemy):
+
+    def __init__(self):
+        super().__init__('Alien', 5)
+
+
+m = Monster()
+a = Alien()
+
+while True:
+    x = input('Please select from "laser", "gun", "exit" ')
+    if x == 'exit':
+        break
+    elif x == 'laser':
+        a.hit()
+    elif x == 'gun':
+        m.hit()
+    else:
+        print('Incorrect input. Please select from "laser", "gun", "exit"')
+
