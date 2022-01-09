0b05199af80a049d7b71b2df0958dc972b993582
Susanna-Salata <susanna.kiev@gmail.com>
2022-01-09 16:45:47 +0100

    Shooting Game

---
+++ recursive function.py
@@ -0,0 +1,13 @@
+
+def spell(txt):
+
+    if len(txt) == 0:
+        return txt
+    else:
+        return spell(txt[1:])+txt[0]
+
+
+txt = input("Given a string: ")
+spell(txt)
+
+print(spell(txt))
