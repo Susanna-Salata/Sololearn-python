fa763232cdcb6f9a66f4d0863cbc0f6b4505d37a
Susanna-Salata <susanna.kiev@gmail.com>
2022-01-13 12:51:59 +0100

    Shooting Game

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+num = int(input())
+
+
+def fibonacci(n):
+    def core(f):
+        if f < 1:
+            return 0
+        elif f <= 2:
+            return 1
+        else:
+            return core(f-1) + core(f-2)
+
+    for i in range(n):
+        print(core(i))
+
+fibonacci(num)
