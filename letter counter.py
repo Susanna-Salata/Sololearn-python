c5004f921dcb759ca75acdeb55e964f5505003eb
Susanna-Salata <susanna.kiev@gmail.com>
2022-01-13 10:16:17 +0100

    Shooting Game

---
+++ letter counter.py
@@ -0,0 +1,14 @@
+text = input()
+dict = {}
+
+for letter in text:
+    if dict.get(letter):
+        i = dict.get(letter) + 1
+    else:
+        i = 1
+
+    dict.update({letter: i})
+
+print(dict)
+
+
